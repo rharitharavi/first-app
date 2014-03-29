52fd98139ac2908ea25e2da835777adc9f6dc673
rharitharavi <rharitharavi@gmail.com>
2014-03-29 17:10:21 -0500

    Depot Scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Product.delete_all
 Product.create!(title: 'Programming Ruby 1.9 & 2.0',
   description:
     %{<p>
