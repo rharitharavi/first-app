22f589fd54c86a2ee8416aab2b87431ec71f1349
rharitharavi <rharitharavi@gmail.com>
2014-03-31 01:21:32 -0500

    Depot Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323171224) do
+ActiveRecord::Schema.define(version: 20140331004946) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
