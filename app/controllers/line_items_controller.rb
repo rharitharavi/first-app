d65f54a9f7a6161dad61b5e5ab3372e89b7ff410
rharitharavi <rharitharavi@gmail.com>
2014-04-05 22:59:50 -0500

    Depot Scaffold

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,8 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to store_url}
-	format.js
+        format.html { redirect_to store_url }
+        format.js   { @current_item = @line_item }
         format.json { render action: 'show',
           status: :created, location: @line_item }
       else
@@ -48,7 +48,7 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item }
+        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
